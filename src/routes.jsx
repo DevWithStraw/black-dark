ab1e3aa08244088fbac9fc7e1d93e2fbdf9cb3d8
proxiner <devwithstraw@gmail.com>
2024-12-09 20:12:45 +0330

    imported new route

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,13 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Favicon from "./helpers/favicon";
 import Home from "./pages/home";
+import NotFound from "./pages/404";
 
-function App() {
+function App() {  
   return (
     <BrowserRouter>
       <Favicon />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
