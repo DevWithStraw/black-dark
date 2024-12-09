ec81f227c53a2b0f9f78d99070785f748c87dca2
proxiner <devwithstraw@gmail.com>
2024-12-09 21:05:15 +0330

    home component is now empty

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -4,7 +4,7 @@ import "./home.scss";
 export default function home() {
   return (
     <div className="welcome">
-      <h1>Welcome To the Black Dark Project!</h1>
+      {/* <h1>Welcome To the Black Dark Project!</h1> */}
     </div>
   );
 }
